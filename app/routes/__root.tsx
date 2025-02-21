057eb4d993b29115c3a7e7160b83c853a56306b0
Antonio Blazquez <antonio.blazquez@immensegroup.com>
2025-02-21 16:39:53 +0100

    install tailwind+shadcn

---
+++ app/routes/__root.tsx
@@ -0,0 +1,58 @@
+// app/routes/__root.tsx
+import type { ReactNode } from 'react'
+import {
+    Outlet,
+    createRootRoute,
+    HeadContent,
+    Scripts,
+} from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+
+import appCss from "@/styles/app.css?url"
+
+export const Route = createRootRoute({
+    head: () => ({
+        meta: [
+            {
+                charSet: 'utf-8',
+            },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+            },
+            {
+                title: 'TanStack Start Starter',
+            },
+        ],
+        links: [
+            {
+                rel: "stylesheet",
+                href: appCss,
+            },
+        ],
+    }),
+    component: RootComponent,
+})
+
+function RootComponent() {
+    return (
+        <RootDocument>
+            <Outlet />
+            <TanStackRouterDevtools />
+        </RootDocument>
+    )
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+    return (
+        <html>
+            <head>
+                <HeadContent />
+            </head>
+            <body>
+                {children}
+                <Scripts />
+            </body>
+        </html>
+    )
+}
